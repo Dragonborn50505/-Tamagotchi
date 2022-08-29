d7c12d5eec48142a1f4fdc406bed9afc5b61ec2f
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2022-08-29 13:42:44 +0200

    first commit

---
+++ ​Tamagotchi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs; //need to instal "Raylib_cs" on every procekt using "NuGetGallery"
+
+
+
+Raylib.InitWindow(800, 600, "hej");
+Raylib.SetTargetFPS(60);
+
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+
+    Raylib.ClearBackground(Color.WHITE);
+    
+    
+    ​Tamagotchi T1;
+
+    T1 = new ​Tamagotchi();
+
+
+    Console.WriteLine(T1);
+
+
+
+    Raylib.EndDrawing();
+}
---
+++ ​Tamagotchi/​Tamagotchi.cs
@@ -0,0 +1,5 @@
+public class ​Tamagotchi
+{
+    Raylib.DrawCircle(100, 100, 100, Color.MAGENTA);
+
+}
