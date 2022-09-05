c072db9a76a01e58e5adf8fc24be4321f91bcc44
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2022-09-05 13:51:40 +0200

    wha

--- ​Tamagotchi/Program.cs
+++ ​Tamagotchi/Program.cs
@@ -2,11 +2,35 @@
 using System.Numerics;
 using Raylib_cs; //need to instal "Raylib_cs" on every procekt using "NuGetGallery"
 
-
+Console.WriteLine("Guten tag, welcome to Tamagotchi");
 Tamagotchi T1 = new Tamagotchi();
+Console.WriteLine("Chose a name of your Tamagotchi");
+T1.name = Console.ReadLine();
+Console.WriteLine($"{T1.name}");
+
+while (T1.GetAlive() == true)
+{
+    Console.Clear();
+    T1.stats();
+    Console.WriteLine("Now what do you want to do?");
+    Console.WriteLine($"1. Teach {T1.name} a new word");
+    Console.WriteLine($"2. Talk to {T1.name}");
+    Console.WriteLine($"3. Feed {T1.name}");
+    Console.WriteLine($"4. Do nothing");
+    string whatDo = Console.ReadLine();
+
+    if (whatDo == "1")
+    {
+
+    }
 
 
-T1.hunger -= 1;
 
-Console.WriteLine($"T1 hunger: {T1.hunger}");
+
+}
+
+
+
+
+Console.ReadLine();
 
--- ​Tamagotchi/​Tamagotchi.cs
+++ ​Tamagotchi/​Tamagotchi.cs
@@ -2,18 +2,64 @@
 public class Tamagotchi
 {
     public int hunger;
-    public string name = "Tammy";
+    public string name;
+    private Random generator;
+    private bool isAlive;
+
+    public int bored;
+
+  
+
+    public Tamagotchi ()
+    {
+        generator = new Random();
+        isAlive = true;
+    }
+
+    public void playing()
+    {
+        bored =-2;
+        if (bored <= 0)
+        {
+            bored = 0;
+        }
+    }
 
 
 
+    public void eating()
+    {
+        hunger =-3;
+        if (hunger <= 0)
+        {
+            hunger = 0;
+        } 
+    }
 
+    
+    public bool GetAlive()
+    {
+        return  isAlive;
+    }
 
     public void timmer()
     {
         hunger++;
+        bored++;
 
+            if (hunger == 10 !& bored == 0)
+            {
+                isAlive = false;
+            }
     }
 
+
+
+
+    public void stats()
+    {
+        Console.WriteLine($"{name} ------------- {hunger} ------------- {bored}");
+    }
 
 
 }
